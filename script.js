cbd46cf574597e9733c023373cf43d371cc58b31
Rok Slana <rokslana@gmail.com>
2022-10-27 10:52:25 +0200

    basic layout

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("test");
+
+const Board = () => {
+    const boardCells = [];
+    const boardField = document.querySelector('board');
+    const renderCells = () =>{
+
+    };
+}
+
+const Player = (name, type) => {
+    const getName = () => name;
+    const getType = () => type;
+    const sayName = () => console.log(`${name} is on the move`);
+    return{sayName};
+}
+
+const newGame = (()=>{
+    //let players = [];
+    const player1 = Player('Rok');
+    const player2 = Player('Rok2');
+
+
+
+    player1.sayName();
+    player2.sayName();
+    // let board = 
+
+})();
